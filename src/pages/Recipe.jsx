c6a47db7d66ad3683373fdb8fac31eb709ef1b86
pigji <wjlqlrll@naver.com>
2025-01-14 14:24:53 +0900

    build

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -37,71 +37,68 @@ const Recipe = () => {
 
   return (
     <div className='recipeDetail'>
-      <img src={ATT_FILE_NO_MK} alt={RCP_NM} className='recipeDetailMainImg'/>
-      <div className='recipeDetailTitle'>▷ {RCP_NM} ◁</div>
+      <img src={ATT_FILE_NO_MK} alt={RCP_NM} className='recipeDetailMainImg' />
+      <div className="recipeDetailTitle">{RCP_NM}</div>
       <div className="recipeDetailInfo">
         <div className="info-title">
           재료
         </div>
         {
-          source.map((el, idx) => (
+          source.map((el,idx)=>(
             <div key={idx} className='txt'>
-              <span className='source-tit'>{el[0]}</span>
-              <span className='source-con'>{el[1]}</span>
+              <span className="source-tit">{el[0]}</span>
+              <span className="source-con">{el[1]}</span>
             </div>
           ))
         }
+
+        
       </div>
-      <div className='recipeDetailInfo1'>
+      <div className="recipeDetailInfo1">
           <div className="info-title1">
-            조리방법
+            조리법
           </div>
           {
-            manuals.map((manual, index) => manual.desc ? (
+            manuals.map((manual, index)=>manual.desc? (
               <div key={index} className='desc_list'>
-                <span className='desc-txt'>{manual.desc}</span>
+                <span className="desc-txt">{manual.desc}</span>
                 <img src={manual.img} alt={RCP_NM} />
               </div>
-            ) : null)
+            ):null)
           }
         </div>
-        <div className="recipeDetailInfo2" >
+        <div className="recipeDetailInfo2">
           <div className="info-title2">
             영양정보
           </div>
           <div className="table">
             <div className="row">
-              <span className='col'>열량 : </span>
-              <span className='col'>{INFO_ENG}</span>
+              <span className="col">열 량 : </span>
+              <span className="col">{INFO_ENG} kal</span>
             </div>
             <div className="row">
-              <span className='col'>탄수화물 : </span>
-              <span className='col'>{INFO_CAR}</span>
+              <span className="col">탄 수 화 물 : </span>
+              <span className="col">{INFO_CAR} g</span>
             </div>
             <div className="row">
-              <span className='col'>단백질 : </span>
-              <span className='col'>{INFO_PRO}</span>
+              <span className="col">단 백 질 : </span>
+              <span className="col">{INFO_PRO} g</span>
             </div>
             <div className="row">
-              <span className='col'>열량 : </span>
-              <span className='col'>{INFO_ENG}</span>
+              <span className="col">지 방 : </span>
+              <span className="col">{INFO_FAT} g</span>
             </div>
             <div className="row">
-              <span className='col'>지방 : </span>
-              <span className='col'>{INFO_FAT}</span>
+              <span className="col">나 트 륨 : </span>
+              <span className="col">{INFO_NA} g</span>
             </div>
-            <div className="row">
-              <span className='col'>나트륨 : </span>
-              <span className='col'>{INFO_NA}</span>
-            </div>
-            <div className='tip'>
-              <span className='col'>조리법 TIP : </span>
-              <span className='col'>{RCP_NA_TIP}</span>
+            <div className="tip">
+              <span className="col">조리법 TIP :</span>
+              <span className="col">{RCP_NA_TIP}</span>
             </div>
           </div>
         </div>
     </div>
-    
   );
 };
 
